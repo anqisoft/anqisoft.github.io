f8c38546218add680d0c6744403f206aea25dd7a
anqisoft <anqisoft@gmail.com>
2024-06-01 17:24:01 +0800

    Also add a set of auxiliary buttons at the beginning; update index.htm to correct the content.

--- assets/js/common.js
+++ assets/js/common.js
@@ -12,17 +12,24 @@ function appendChangeLangArea() {
 		return;
 	}
 
-	var changeLangArea = document.createElement('div');
-	changeLangArea.innerHTML = `
-			<en_us>If you want to change the language, please click the button.</en_us>
-			<zh_cn>如果您想切换语言，请点击下一行相应按钮。</zh_cn>
-			<zh_tw>如果您想切換語言，請點擊下一行相應按鈕。</zh_tw>
-			<br />
-			<button type="button" onclick="setLang('en_us')"><en_us>En</en_us><zh_cn>Chinese</zh_cn><zh_tw>Traditional</zh_tw></button>
-			<button type="button" onclick="setLang('zh_cn')"><en_us>英语</en_us><zh_cn>简体</zh_cn><zh_tw>繁体</zh_tw></button>
-			<button type="button" onclick="setLang('zh_tw')"><en_us>英語</en_us><zh_cn>簡體</zh_cn><zh_tw>繁體</zh_tw></button>`;
+	for (let firstOrLastLoop = 0; firstOrLastLoop < 2; ++firstOrLastLoop) {
+		var changeLangArea = document.createElement('div');
+		changeLangArea.innerHTML = `
+				<en_us>If you want to change the language, please click the button.</en_us>
+				<zh_cn>如果您想切换语言，请点击下一行相应按钮。</zh_cn>
+				<zh_tw>如果您想切換語言，請點擊下一行相應按鈕。</zh_tw>
+				<br />
+				<button type="button" onclick="setLang('en_us')"><en_us>En</en_us><zh_cn>Chinese</zh_cn><zh_tw>Traditional</zh_tw></button>
+				<button type="button" onclick="setLang('zh_cn')"><en_us>英语</en_us><zh_cn>简体</zh_cn><zh_tw>繁体</zh_tw></button>
+				<button type="button" onclick="setLang('zh_tw')"><en_us>英語</en_us><zh_cn>簡體</zh_cn><zh_tw>繁體</zh_tw></button>`;
 
-	body.appendChild(changeLangArea);
+		if (firstOrLastLoop) {
+			body.appendChild(changeLangArea);
+		} else {
+			body.insertBefore(changeLangArea, body.children[0]);
+		}
+
+	}
 }
 
 appendChangeLangArea();
